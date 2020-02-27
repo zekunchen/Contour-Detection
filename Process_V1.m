2bf3db8f7134b30919a5f38566b46a40c6b7f226
zekunchen <229800776@qq.com>
2020-02-27 19:27:10 +0800

    Update Process_V1.m

--- Process_V1.m
+++ Process_V1.m
@@ -2,5 +2,5 @@ function [TT, Tt] =Process_V1(outm)
     [Responset, Orientationst] = max(outm, [], 1);
     Response(:,:,:)=Responset(1,1,:,:,:);
     Orientations(:,:,:)=Orientationst(1,1,:,:,:);
-    [TT, Tt] = CollapseChannels(Response, Orientations);
+    [TT, Tt] = ChannelMax(Response, Orientations);
 end
